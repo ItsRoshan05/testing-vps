b6bebecfadae2d241c460839b430a5463534efcd
itsRoshan05 <yokim051019@gmail.com>
2025-08-09 10:29:52 +0700

    update fetch data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8000") // nanti ini ganti ke URL backend di VPS
+    fetch("http://103.187.146.20/api/v1/") // nanti ini ganti ke URL backend di VPS
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch((err) => console.error(err));
